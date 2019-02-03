4ba37bf57db11f233fab336fefdd6b5b6e400b63
SwiCago <al@swicago.com>
2019-02-03 10:04:13 -0600

    Update SAUNA.h

--- src/SAUNA.h
+++ src/SAUNA.h
@@ -1,3 +1,18 @@
+/*
+  SAUNA.h - Harvia sauna heater control code for Sonoff TH esp8266 based devices.
+  Copyright (c) 2019 Al Betschart.  All right reserved.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 const char* setup_page = "<html>"
                          "<head>"
                          "<meta charset='utf-8'/>"
